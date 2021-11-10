f952f68c27d9718c6ece955a92de5a3c54e850a6
jainnishkarsh18 <jainnishkarsh18@gmail.com>
2021-11-10 19:23:53 +0530

    final update

--- js/contact-us.js
+++ js/contact-us.js
@@ -1,3 +1,27 @@
 function validateform() {
-    let x =
+
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    let name = document.forms["contact"]["name"].value;
+    if (name == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+    if (name.length <= 3) {
+        alert("name should be greater than 3 letter");
+        return false;
+    }
+    let number = document.forms["contact"]["number"].value;
+    if (number.length > 10 || number.length < 10) {
+        alert("invalid number");
+        return false;
+    }
+    let email = document.forms["contact"]["email-id"].value;
+    if (email.match(mailformat)) {
+        return true;
+    } else {
+        alert("Invalid email");
+        return false;
+    }
+
 }
